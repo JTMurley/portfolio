69d61cf28497efac332e39f263f04753a1941db3
Jack Murley <jack.murley@gmail.com>
2025-07-11 11:02:17 +1000

    feat(portfolio): barebones cms integrated

--- portfolio/src/pages/index.js
+++ portfolio/src/pages/index.js
@@ -212,7 +212,7 @@ const projects = [
   {
     title: 'Portfolio Website',
     description: 'This website, built with 💖',
-    tech: ['React', 'Docusaurus ', 'Decap CMS', 'Algolia', 'PostHog Cloud', 'Cloudflare'],
+    tech: ['React', 'Docusaurus ', 'Sveltia CMS', 'Algolia', 'PostHog Cloud', 'Cloudflare'],
     link: 'https://jtmurley.github.io/portfolio/',
     github: 'https://github.com/JTMurley/portfolio'
   },
